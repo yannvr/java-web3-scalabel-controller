7263e94f62051f835571e8bccfa559cda150c700
Yann VR <yannvr@gmail.com>
2025-03-26 14:04:09 +0000

    feat: Add Caffeine caching support and enable caching in application
    - Add Caffeine dependency for caching capabilities
    - Enable caching in the DemoApplication class to improve performance

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableRabbit
+@EnableCaching
 public class DemoApplication {
 
 	public static void main(String[] args) {
